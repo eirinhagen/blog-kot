e5fade3131f7766d99f713ce758d11b4bb52fe25
Jane <evgeniiatsyp13@gmail.com>
2023-02-23 14:28:53 +0100

    Added  new code to [slag].tsx from the Sanity tutoral

--- frontend/src/pages/post/[slug].tsx
+++ frontend/src/pages/post/[slug].tsx
@@ -1,41 +1,39 @@
-// post.tsx
-import { client } from '@/client'
-import { useRouter } from 'next/router'
+// [slug].tsx
 
-const Post: React.FC<any> = ({post}) => {
+import {client} from '../../client'
 
-  console.log(post);
+const Post = (props: any) => {
+  const { title = 'Missing title', name = 'Missing name' } = props.post
   return (
     <article>
-      <h1>{post?.slug?.current}</h1>
+      <h1>{title}</h1>
+      <span>By {name}</span>
     </article>
   )
 }
 
 export async function getStaticPaths() {
-    const paths = await client.fetch(
-      `*[_type == "post" && defined(slug.current)][].slug.current`
-    )
-  
-    return {
-      paths: paths.map((slug: any) => ({params: {slug}})),
-      fallback: true,
+  const paths = await client.fetch(
+    `*[_type == "post" && defined(slug.current)][].slug.current`
+  )
+
+  return {
+    paths: paths.map((slug: any) => ({params: {slug}})),
+    fallback: true,
+  }
+}
+
+export async function getStaticProps(context: any) {
+  // It's important to default the slug so that it doesn't return "undefined"
+  const { slug = "" } = context.params
+  const post = await client.fetch(`
+    *[_type == "post" && slug.current == $slug][0]{title, "name": author->name}
+  `, { slug })
+  return {
+    props: {
+      post
     }
   }
-  
-  export async function getStaticProps(context: any) {
-    // It's important to default the slug so that it doesn't return "undefined"
-    const { slug = "" } = context.params
-    const post = await client.fetch(`
-      *[_type == "post" && slug.current == $slug][0]
-    `, { slug })
-    
-    return {
-      props: {
-        post
-      }
-    }
-  }
-  
+}
 
 export default Post
